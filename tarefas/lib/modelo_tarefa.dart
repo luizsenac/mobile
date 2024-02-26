d3ff47f4360bb87c42797fac54ce6887f3dd0cc2
Luiz Gustavo Francisco <luiz.francisco@docente.pr.senac.br>
2024-02-26 15:08:01 -0300

    criação de app de tarefas

---
+++ tarefas/lib/modelo_tarefa.dart
@@ -0,0 +1,8 @@
+// ignore: camel_case_types
+class Modelo_tarefa {
+  String titulo;
+  bool completa;
+
+  //método construtor, cria um objeto para o uso
+  Modelo_tarefa({required this.titulo, this.completa = false});
+}
